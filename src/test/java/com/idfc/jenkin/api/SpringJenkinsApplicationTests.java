9e4d23aa513b73935a40a50b8489ae3c9dca7028
unknown <kishore.pami@gmail.com>
2023-01-14 18:07:41 +0530

    initial commit

---
+++ src/test/java/com/idfc/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.idfc.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+
+	@Test
+	void contextLoads() {
+		logger.info("Test cases are executed....");
+		assertEquals(true, true);
+	
+	}
+
+}
